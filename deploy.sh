1e1c436882c8468908b2829712b7c03c202fa33f
Giovanni Baleani <giovibal@users.noreply.github.com>
2016-08-19 12:11:55 +0200

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mvn -DskipTests clean package
-
-scp target/mqtt-tester-1.0.1.jar smartpl01:/opt/smart-platform/mqtt-tester-1.0.1.jar
-scp target/mqtt-tester-1.0.1.jar smartpl02:/opt/smart-platform/mqtt-tester-1.0.1.jar
-scp target/mqtt-tester-1.0.1.jar smartpl07:/opt/smart-platform/mqtt-tester-1.0.1.jar
